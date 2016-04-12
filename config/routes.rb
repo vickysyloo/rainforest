17b0c7ca71670576022348d3ade45e78fc2ddfc0
Vicky L <vicky.sy.loo@gmail.com>
2016-04-11 22:16:35 -0400

    reached part 13

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
 root 'products#index'
 
-resources :products
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
 resources :users, only: [:new, :create]
 resources :sessions, only: [:new, :create, :destroy]
 
