94238b55f3e95b49b8d30a511de930e2a1a5cccc
Vicky L <vicky.sy.loo@gmail.com>
2016-04-27 14:53:20 -0400

    practicing adding AJAX for reviews

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_path, notice: "Logged in!"
+      redirect_to root_path, notice: "Logged in!"
     else
       flash.now[:alert] = "Invalid name or password"
       render "new"
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to products_url, notice: "Logged out!"
+    redirect_to root_url, notice: "Logged out!"
   end
 end
