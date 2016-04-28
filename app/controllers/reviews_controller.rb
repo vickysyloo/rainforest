9511bea7d6863eafa4e163ca0ecbc423a9931965
Vicky L <vicky.sy.loo@gmail.com>
2016-04-28 16:06:41 -0400

    added ajax for rendering reviews and errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,10 +10,14 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.build(review_params)
     @review.user = current_user
 
-    if @review.save
-      redirect_to products_path, notice: "Review created successfully"
-    else
-      render "products/show"
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to products_path, notice: "Review created successfully." }
+        format.js{} # This will look for app/views/reviews/create.js.erb
+      else
+        format.html { render "products/show", alert: "Error!" }
+        format.js{}
+      end
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  validates :comment, :presence => true
+
 end
